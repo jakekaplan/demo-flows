ff6ee6f3e5966d779e1d3a3d9acb6aa237f8bc0a
jake@prefect.io <jake@prefect.io>
2023-02-21 18:29:24 -0500

    update hello flow

--- hello_flow.py
+++ hello_flow.py
@@ -1,9 +1,13 @@
+import sys
+import prefect
 from prefect import flow
 
 
 @flow(log_prints=True)
 def hi():
     print("Hi from Prefect! 🤗")
+    print(f"Prefect Version: {prefect.__version__}")
+    print(f"Python Version: {sys.version}")
 
 
 if __name__ == "__main__":

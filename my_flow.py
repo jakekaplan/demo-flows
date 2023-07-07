5b7a47788ee4d2af4da89d8e311cf8a67ff75f25
jake@prefect.io <jake@prefect.io>
2023-07-07 10:41:09 -0400

    sleep for longer

--- my_flow.py
+++ my_flow.py
@@ -3,11 +3,10 @@ import time
 
 @flow
 def my_flow():
-    print("I will sleep for 10 seconds.")
-    time.sleep(30)
+    print("I will sleep for 60 seconds.")
+    time.sleep(60)
     print("I'm done!")
 
 
 if __name__ == '__main__':
-    raise BaseException
     my_flow()

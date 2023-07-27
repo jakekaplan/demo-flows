e9b7f851a3d8008691527f2b1ec140856e6ca23b
jake@prefect.io <jake@prefect.io>
2023-07-26 21:32:20 -0400

    my_flow.py

--- my_flow.py
+++ my_flow.py
@@ -1,10 +1,10 @@
 from prefect import flow
 import time
-
+from prefect.cli.deployment import run
 @flow(log_prints=True)
 def my_flow():
-    print("I will sleep for 15 seconds.")
-    time.sleep(15)
+    print("I will sleep for 60 seconds.")
+    time.sleep(60)
     print("I'm done!")
 
 

47240e8a4a62c7045a17d3b7276ff6db710f6603
jake@prefect.io <jake@prefect.io>
2023-09-14 08:13:52 -0400

    failed

--- my_flow.py
+++ my_flow.py
@@ -6,10 +6,7 @@ from prefect.runtime import flow_run
 
 @flow(log_prints=True)
 def my_flow():
-    print(f"Starting flow_run: {flow_run.name}")
-    time.sleep(15)
-    print(f"Finished flow_run: {flow_run.name}")
-
+    raise ValueError("Failed")
 
 if __name__ == '__main__':
     my_flow()

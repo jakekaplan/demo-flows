4b741a8d61a58c8eb8a503cfe2a2fa3e310ae4c0
jake@prefect.io <jake@prefect.io>
2023-09-15 10:07:58 -0400

    my flow

--- my_flow.py
+++ my_flow.py
@@ -3,11 +3,10 @@ import time
 from prefect import flow
 from prefect.runtime import flow_run
 
-
 @flow(log_prints=True)
 def my_flow():
     print(f"Starting flow_run: {flow_run.name}")
-    time.sleep(300)
+    time.sleep(15)
     print(f"Finished flow_run: {flow_run.name}")
 
 

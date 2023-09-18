19291ca51fe6e2c7bacebc7461417b11db522c49
jake@prefect.io <jake@prefect.io>
2023-09-18 10:50:55 -0400

    oom error

--- my_flow.py
+++ my_flow.py
@@ -2,12 +2,18 @@ import time
 
 from prefect import flow
 from prefect.runtime import flow_run
+from prefect_aws.workers.ecs_worker import ECSWorker
+
+
 
 @flow(log_prints=True)
 def my_flow():
-    print(f"Starting flow_run: {flow_run.name}")
-    time.sleep(15)
-    print(f"Finished flow_run: {flow_run.name}")
+    x = []
+    while True:
+        x.append(1)
+    # print(f"Starting flow_run: {flow_run.name}")
+    # time.sleep(15)
+    # print(f"Finished flow_run: {flow_run.name}")
 
 
 if __name__ == '__main__':

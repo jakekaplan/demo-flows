f2562a8a9b6733621537fee0e18de5aeefb3931d
jake@prefect.io <jake@prefect.io>
2023-09-19 15:58:55 -0400

    normal flow

--- my_flow.py
+++ my_flow.py
@@ -4,10 +4,9 @@ from prefect import flow
 from prefect.runtime import flow_run
 @flow(log_prints=True)
 def my_flow():
-    oom_maker = []
-    while True:
-        oom_maker.append('O'*1024**2)
-
+    print(f"Starting flow run: {flow_run.name}")
+    time.sleep(15)
+    print(f"Finishing flow run: {flow_run.name}")
 
 
 if __name__ == '__main__':

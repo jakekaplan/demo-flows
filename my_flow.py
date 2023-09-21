027f73de702c9040b9ff1d0c3c6278d3d4ae3956
jake@prefect.io <jake@prefect.io>
2023-09-21 08:04:03 -0400

    regular ass flow

--- my_flow.py
+++ my_flow.py
@@ -4,12 +4,9 @@ from prefect import flow
 from prefect.runtime import flow_run
 @flow(log_prints=True)
 def my_flow():
-    x = []
-    while True:
-        x.append("aaaaaaa" * 100000000)
-    # print(f"Starting flow run: {flow_run.name}")
-    # time.sleep(10)
-    # print(f"Finishing flow run: {flow_run.name}")
+    print(f"Starting flow run: {flow_run.name}")
+    time.sleep(10)
+    print(f"Finishing flow run: {flow_run.name}")
 
 
 if __name__ == '__main__':

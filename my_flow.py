4e1c11a18a61fa396792eab687acbf5eead4ff66
jake@prefect.io <jake@prefect.io>
2023-09-21 04:59:40 -0400

    push

--- my_flow.py
+++ my_flow.py
@@ -2,10 +2,12 @@ import time
 
 from prefect import flow
 from prefect.runtime import flow_run
+from prefect_gcp.worker import CloudRunWorker
+
 @flow(log_prints=True)
 def my_flow():
     print(f"Starting flow run: {flow_run.name}")
-    time.sleep(60)
+    time.sleep(10)
     print(f"Finishing flow run: {flow_run.name}")
 
 

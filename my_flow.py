11d0976de4d9263e7b67ba5196df1b7fd3d53c6b
jake@prefect.io <jake@prefect.io>
2023-09-21 07:28:35 -0400

    oom flow

--- my_flow.py
+++ my_flow.py
@@ -5,9 +5,12 @@ from prefect.runtime import flow_run
 
 @flow(log_prints=True)
 def my_flow():
-    print(f"Starting flow run: {flow_run.name}")
-    time.sleep(10)
-    print(f"Finishing flow run: {flow_run.name}")
+    x = []
+    while True:
+        x.append("aaaaa" * 100000000)
+    # print(f"Starting flow run: {flow_run.name}")
+    # time.sleep(60)
+    # print(f"Finishing flow run: {flow_run.name}")
 
 
 if __name__ == '__main__':

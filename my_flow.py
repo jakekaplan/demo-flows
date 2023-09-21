d78f4f365fab48f204b1d4c735dfee345367c01a
jake@prefect.io <jake@prefect.io>
2023-09-21 07:07:14 -0400

    good flow

--- my_flow.py
+++ my_flow.py
@@ -1,10 +1,14 @@
+import time
+
 from prefect import flow
+from prefect.runtime import flow_run
 
 @flow(log_prints=True)
 def my_flow():
-    x = []
-    while True:
-        x.append("a" * 1000000)
+    print(f"Starting flow run: {flow_run.name}")
+    time.sleep(10)
+    print(f"Finishing flow run: {flow_run.name}")
+
 
 if __name__ == '__main__':
     my_flow()

f7e42e8e6fc7225cbab6712daab4a095102d9a34
jake@prefect.io <jake@prefect.io>
2023-09-28 21:00:28 -0400

    push

--- my_flow.py
+++ my_flow.py
@@ -2,11 +2,13 @@ import time
 
 from prefect import flow
 from prefect.runtime import flow_run
+from prefect.runtime import deployment
 
 
 @flow(log_prints=True)
 def my_flow():
     print(f"Starting flow run: {flow_run.name}")
+    print(f"Deployment {deployment.name}")
     time.sleep(10)
     print(f"Finishing flow run: {flow_run.name}")
 

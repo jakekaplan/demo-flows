17aa104d97312082c128188cb1eb3b254fb2cac2
jake@prefect.io <jake@prefect.io>
2023-10-03 08:47:59 -0400

    temp tasks

--- my_flow.py
+++ my_flow.py
@@ -1,20 +1,19 @@
-import sys
-import time
+from prefect import task, flow
 
-from prefect import flow
-from prefect.runtime import flow_run
-from prefect.runtime import deployment
+@task()
+def task1():
+    print("trying task 1")
 
-
-
-@flow(log_prints=True)
-def my_flow():
-    print(sys.version)
-    # print(f"Starting flow run: {flow_run.name}")
-    # print(f"Deployment {deployment.name}")
-    # time.sleep(10)
-    # print(f"Finishing flow run: {flow_run.name}")
+@task()
+def task2():
+    print("trying task 2")
+    raise ValueError("fail")
 
 
-if __name__ == '__main__':
-    my_flow()
+@flow(retries=2, log_prints=True)
+def run_tasks():
+    task1()
+    task2()
+
+if __name__ == "__main__":
+    run_tasks()

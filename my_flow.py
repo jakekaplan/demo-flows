12ae49fdd08fed67e6130aa90c501712a62af19e
jake@prefect.io <jake@prefect.io>
2023-10-13 10:40:18 -0400

    back to normal

--- my_flow.py
+++ my_flow.py
@@ -1,16 +1,11 @@
-import sys
 import time
 
 from prefect import flow, task
-from prefect.runtime import flow_run
-from prefect.runtime import deployment
-from prefect.client.orchestration import get_client
+
 
 @task
 def my_task():
-    time.sleep(99999)
-    # raise ValueError("This is a test")
-
+    time.sleep(15)
 
 @flow
 def my_flow():

5ebeb3ac1d79427f6d97aa0c7fdff114a914fae6
jake@prefect.io <jake@prefect.io>
2023-10-14 13:18:10 -0400

    what python

--- my_flow.py
+++ my_flow.py
@@ -3,13 +3,13 @@ import time
 from prefect import flow, task
 
 
-@task
+@task(log_prints=True)
 def my_task():
     import sys
     print(sys.version)
 
 
-@flow
+@flow(log_prints=True)
 def my_flow():
     my_task()
 

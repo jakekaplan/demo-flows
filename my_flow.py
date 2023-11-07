3a02ea3138f14229e22e6719ccb4aeca4c22df74
jake@prefect.io <jake@prefect.io>
2023-11-07 16:54:53 -0500

    demo flows

--- my_flow.py
+++ my_flow.py
@@ -1,14 +1,8 @@
-import time
-
-from prefect import flow, task
-import os
+from prefect import flow
 
 @flow(log_prints=True)
 def my_flow():
-    test1 = os.getenv("TEST1", "test1 not set")
-    print(test1)
-    test2 = os.getenv("TEST2", "test2 not set")
-    print(test2)
+    print(1)
 
 
 if __name__ == '__main__':

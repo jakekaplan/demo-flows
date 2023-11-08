abc89320c5e059bf30cb3b267b9b2a2530282b07
jake@prefect.io <jake@prefect.io>
2023-11-07 21:29:40 -0500

    pandddasz

--- my_flow.py
+++ my_flow.py
@@ -1,9 +1,14 @@
-from prefect import flow
+from prefect import flow, Flow
+import pandas as pd
 
 @flow(log_prints=True)
 def my_flow():
-    print(1)
+    df = pd.DataFrame({"a": [1, 2, 3]})
+    print(df)
 
 
 if __name__ == '__main__':
-    my_flow()
+    flow.from_source(
+        "https://github.com/jakekaplan/demo-flows.git",
+        entrypoint="my_flow.py:my_flow"
+    ).deploy(name="jake-mex-deployment", work_pool_name="prefect-managed")

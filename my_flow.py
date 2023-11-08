091ebde8bba2e41a52b7cfef51397b6e9093351a
jake@prefect.io <jake@prefect.io>
2023-11-07 21:59:16 -0500

    sleep for awhile

--- my_flow.py
+++ my_flow.py
@@ -1,14 +1,14 @@
 from prefect import flow, Flow
-import pandas as pd
+import time
 
 @flow(log_prints=True)
 def my_flow():
-    df = pd.DataFrame({"a": [1, 2, 3]})
-    print(df)
+    time.sleep(1000000)
 
 
 if __name__ == '__main__':
-    flow.from_source(
-        "https://github.com/jakekaplan/demo-flows.git",
-        entrypoint="my_flow.py:my_flow"
-    ).deploy(name="jake-mex-deployment", work_pool_name="prefect-managed")
+    my_flow()
+    # flow.from_source(
+    #     "https://github.com/jakekaplan/demo-flows.git",
+    #     entrypoint="my_flow.py:my_flow"
+    # ).deploy(name="jake-mex-deployment", work_pool_name="prefect-managed")

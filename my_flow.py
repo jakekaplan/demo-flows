45e5c105b19f37d7e60d92fac956abcb7cf2472e
jake@prefect.io <jake@prefect.io>
2023-12-14 14:11:12 -0500

    Test timeout

--- my_flow.py
+++ my_flow.py
@@ -5,9 +5,7 @@ from prefect import flow
 @flow(log_prints=True)
 def my_flow():
     print("Hello world!")
-    for i in range(60):
-        print(f"{i}th minute, sleeping for 60 seconds...")
-        time.sleep(60)
+    time.sleep(5)
     print("Goodbye world!")
 
 

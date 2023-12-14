1c4659517082d75d1684a0eb124e303bbf062e7f
jake@prefect.io <jake@prefect.io>
2023-12-14 13:38:41 -0500

    stress test

--- my_flow.py
+++ my_flow.py
@@ -1,15 +1,21 @@
 import time
 from prefect import flow
-import pandas
-import numpy
-import flask
+
 
 @flow(log_prints=True)
 def my_flow():
     print("Hello world!")
-    time.sleep(5)
+    for i in range(60):
+        print(f"{i}th minute, sleeping for 60 seconds...")
+        time.sleep(60)
     print("Goodbye world!")
 
 
-if __name__ == '__main__':
-    my_flow()
+if __name__ == "__main__":
+    flow.from_source(
+        source="https://github.com/jakekaplan/demo-flows.git",
+        entrypoint="my_flow.py:my_flow",
+    ).deploy(
+        name="mex-deployment",
+        work_pool_name="mex-work-pool",
+    )

8c3fed7d18efb86f19715469fd3e3913ddf81e3b
jake@prefect.io <jake@prefect.io>
2023-12-14 08:54:09 -0500

    bye bye now

--- my_flow.py
+++ my_flow.py
@@ -1,29 +1,12 @@
-from prefect import flow
-from prefect.runtime import flow_run
 import time
+from prefect import flow
+
 
 @flow(log_prints=True)
 def my_flow():
-    print(f"I'm in flow: {flow_run.name}. Sleeping for 5...")
+    print("Hello world!")
     time.sleep(5)
-    print("Done!")
-
+    print("Goodbye world!")
 
 if __name__ == '__main__':
     my_flow()
-
-    # flow.from_source(
-    #     "https://github.com/jakekaplan/demo-flows.git",
-    #     entrypoint="my_flow.py:my_flow"
-    # ).serve(
-    #     name="jake-serve",
-    #     interval=15
-    # )
-
-    # flow.from_source(
-    #     "https://github.com/jakekaplan/demo-flows.git",
-    #     entrypoint="my_flow.py:my_flow"
-    # ).deploy(
-    #     name="jake-mex-deployment",
-    #     work_pool_name="prefect-managed"
-    # )

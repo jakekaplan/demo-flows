0389d93f6e4acb2ad9709d7cd393ee2bae26d19a
jake@prefect.io <jake@prefect.io>
2023-12-15 15:01:39 -0500

    my first flow

--- my_flow.py
+++ my_flow.py
@@ -1,22 +1,14 @@
 from prefect import flow
-import pandas as pd
+
 
 @flow(log_prints=True)
 def my_flow():
     print("Hello world!")
 
-    df = pd.DataFrame({'a': [1, 2, 3]})
-    print(f"Here's a dataframe: {df}")
-
-    print("Goodbye world!")
-
-
-if __name__ == "__main__":
-    flow.from_source(
-        source="https://github.com/jakekaplan/demo-flows.git",
-        entrypoint="my_flow.py:my_flow",
-    ).deploy(
-        name="my-managed-deployment",
-        work_pool_name="my-managed-pool",
-        job_variables={"pip_packages": ["pandas"]}
-    )
+my_flow.from_source(
+    source="https://github.com/jakekaplan/demo-flows.git",
+    entrypoint="my_flow.py:my_flow",
+).deploy(
+    name="my-managed-deployment",
+    work_pool_name="my-managed-pool",
+)

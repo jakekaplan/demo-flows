7af98e43c953bb8b242711c8c95a73c3b551e746
jake@prefect.io <jake@prefect.io>
2023-12-15 12:58:50 -0500

    try this

--- my_flow.py
+++ my_flow.py
@@ -1,13 +1,24 @@
 import time
 from prefect import flow
 import pandas as pd
+from marvin import ai_fn
+
+
+@ai_fn
+def quote_marvin(topic: str) -> str:
+    """Quote Marvin the robot from Hitchhiker's Guide on a topic"""
 
 
 @flow(log_prints=True)
 def my_flow():
     print("Hello world!")
+
     df = pd.DataFrame({'a': [1, 2, 3]})
     print(f"Here's a dataframe: {df}")
+
+    quote = quote_marvin(topic="managed execution")
+    print(f"Here's a quote {quote})")
+
     print("Goodbye world!")
 
 

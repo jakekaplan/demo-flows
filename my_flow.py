2765d5b2e72d0af495c90e6242a71a7afa8383a7
jake@prefect.io <jake@prefect.io>
2023-12-15 15:02:14 -0500

    my first flow

--- my_flow.py
+++ my_flow.py
@@ -1,14 +1,1 @@
-from prefect import flow
-
-
-@flow(log_prints=True)
-def my_flow():
-    print("Hello world!")
-
-my_flow.from_source(
-    source="https://github.com/jakekaplan/demo-flows.git",
-    entrypoint="my_flow.py:my_flow",
-).deploy(
-    name="my-managed-deployment",
-    work_pool_name="my-managed-pool",
-)
+hi

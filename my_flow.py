c233fe0f964e4a01150f55d4bab11c01b40c8bf9
jake@prefect.io <jake@prefect.io>
2023-12-15 11:48:15 -0500

    asdfasd

--- my_flow.py
+++ my_flow.py
@@ -4,7 +4,9 @@ from prefect import flow
 
 @flow(log_prints=True)
 def my_flow():
-    raise Exception("NEED YOU TO FAIL")
+    print("Hello world!")
+    time.sleep(10)
+    print("Goodbye world!")
 
 if __name__ == "__main__":
     flow.from_source(
@@ -12,5 +14,5 @@ if __name__ == "__main__":
         entrypoint="my_flow.py:my_flow",
     ).deploy(
         name="mex-deployment",
-        work_pool_name="mex-work-pool",
+        work_pool_name="mex-workpool",
     )

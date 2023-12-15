e669fda092a83953ff468e350ef89d76bb656aef
jake@prefect.io <jake@prefect.io>
2023-12-15 12:51:06 -0500

    do pandas stuff

--- my_flow.py
+++ my_flow.py
@@ -1,18 +1,22 @@
 import time
 from prefect import flow
+import pandas as pd
 
 
 @flow(log_prints=True)
 def my_flow():
     print("Hello world!")
-    time.sleep(10)
+    df = pd.DataFrame({'a': [1, 2, 3]})
+    print(f"Here's a dataframe: {df}")
     print("Goodbye world!")
+
 
 if __name__ == "__main__":
     flow.from_source(
         source="https://github.com/jakekaplan/demo-flows.git",
         entrypoint="my_flow.py:my_flow",
     ).deploy(
-        name="mex-deployment",
-        work_pool_name="mex-workpool",
+        name="my-managed-deployment",
+        work_pool_name="my-managed-pool",
+        job_variables={"pip_packages": ["pandas"]}
     )

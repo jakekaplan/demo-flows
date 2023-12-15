640509fd836b4c56c5b2d4f4082af52c80909ecb
jake@prefect.io <jake@prefect.io>
2023-12-15 13:03:50 -0500

    back to pandas

--- my_flow.py
+++ my_flow.py
@@ -1,13 +1,5 @@
-import time
 from prefect import flow
 import pandas as pd
-from marvin import ai_fn
-
-
-@ai_fn
-def quote_marvin(topic: str) -> str:
-    """Quote Marvin the robot from Hitchhiker's Guide on a topic"""
-
 
 @flow(log_prints=True)
 def my_flow():
@@ -15,9 +7,6 @@ def my_flow():
 
     df = pd.DataFrame({'a': [1, 2, 3]})
     print(f"Here's a dataframe: {df}")
-
-    quote = quote_marvin(topic="managed execution")
-    print(f"Here's a quote {quote})")
 
     print("Goodbye world!")
 

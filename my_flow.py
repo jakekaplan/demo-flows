8d0b8da3d2a29ed1303bdfb00b8feecae2cf90d9
jake@prefect.io <jake@prefect.io>
2024-01-08 11:34:28 -0500

    do some sleeping

--- my_flow.py
+++ my_flow.py
@@ -1,7 +1,11 @@
 from prefect import flow
+import time
 
 
 @flow(log_prints=True)
 def my_flow():
     print("Hello from a flow!")
 
+    for i in range(20):
+        time.sleep(10)
+        print("Slept for 10 seconds")

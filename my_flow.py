b831d4f6134f4ac37152458b5e4aee82c24b9c23
jake@prefect.io <jake@prefect.io>
2024-01-09 13:22:35 -0500

    aaa

--- my_flow.py
+++ my_flow.py
@@ -5,11 +5,8 @@ import time
 
 @flow(log_prints=True)
 def my_flow():
-    x = []
-    while True:
-        x.append(10000000000000000000000000000000000000123)
-    # print("Hello from a flow!")
-    #
-    # for i in range(20):
-    #     time.sleep(10)
-    #     print("Slept for 10 seconds")
+    print("Hello from a flow!")
+
+    for i in range(20):
+        time.sleep(10)
+        print("Slept for 10 seconds")

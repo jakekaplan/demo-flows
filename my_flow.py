823ef9cf009990c951967438f9d868bcf112f364
jake@prefect.io <jake@prefect.io>
2024-01-09 12:42:37 -0500

    oom flow

--- my_flow.py
+++ my_flow.py
@@ -1,11 +1,16 @@
 from prefect import flow
+from prefect.deployments import run_deployment
 import time
 
 
 @flow(log_prints=True)
 def my_flow():
-    print("Hello from a flow!")
+    x = []
+    while True:
+        x.append('999999999999')
 
-    for i in range(20):
-        time.sleep(10)
-        print("Slept for 10 seconds")
+    # print("Hello from a flow!")
+    #
+    # for i in range(20):
+    #     time.sleep(10)
+    #     print("Slept for 10 seconds")

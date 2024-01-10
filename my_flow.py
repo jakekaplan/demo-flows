165972acaa071001bb8d5123fee870376b4e3d80
jake@prefect.io <jake@prefect.io>
2024-01-10 12:14:09 -0500

    hi there

--- my_flow.py
+++ my_flow.py
@@ -1,11 +1,6 @@
 from prefect import flow
-from prefect.deployments import run_deployment
-import time
 
 
 @flow(log_prints=True)
 def my_flow():
-    print("Hello from a flow!")
-
-    time.sleep(10)
-    print("Slept for 10 seconds")
+    print("Hi from a flow")

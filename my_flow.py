d33789804fb06a2fa55109291b5dae2d80e41820
jake@prefect.io <jake@prefect.io>
2024-01-11 15:14:12 -0500

    my modal pool

--- my_flow.py
+++ my_flow.py
@@ -1,20 +1,7 @@
 from prefect import flow
-import flask
+
 
 @flow(log_prints=True)
 def my_flow():
     print("Hi from a flow")
 
-
-# flow.from_source(
-#     "https://github.com/jakekaplan/demo-flows.git",
-#     entrypoint="my_flow.py:my_flow"
-# ).deploy(
-#     name="jake-pip-test",
-#     work_pool_name="cloud-run-v2-push",
-#     job_variables={
-#         "env": {
-#             "EXTRA_PIP_PACKAGES": "flask",
-#         }
-#     }
-# )

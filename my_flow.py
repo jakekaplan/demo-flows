017d4b035172dd2fa4ceb2a183f114a069d5782e
jake@prefect.io <jake@prefect.io>
2024-01-11 14:56:02 -0500

    flask2

--- my_flow.py
+++ my_flow.py
@@ -6,15 +6,15 @@ def my_flow():
     print("Hi from a flow")
 
 
-flow.from_source(
-    "https://github.com/jakekaplan/demo-flows.git",
-    entrypoint="my_flow.py:my_flow"
-).deploy(
-    name="jake-pip-test",
-    work_pool_name="cloud-run-v2-push",
-    job_variables={
-        "env": {
-            "EXTRA_PIP_PACKAGES": "flask",
-        }
-    }
-)
+# flow.from_source(
+#     "https://github.com/jakekaplan/demo-flows.git",
+#     entrypoint="my_flow.py:my_flow"
+# ).deploy(
+#     name="jake-pip-test",
+#     work_pool_name="cloud-run-v2-push",
+#     job_variables={
+#         "env": {
+#             "EXTRA_PIP_PACKAGES": "flask",
+#         }
+#     }
+# )

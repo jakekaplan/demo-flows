90b4586f88776cc036ce2261b39707e844ac8075
jake@prefect.io <jake@prefect.io>
2024-01-12 09:30:51 -0500

    environ

--- my_flow.py
+++ my_flow.py
@@ -1,7 +1,16 @@
 from prefect import flow
-
+import os
 
 @flow(log_prints=True)
 def my_flow():
-    print("Hi from a flow")
+    print(os.environ)
+    print("Hello world!")
 
+#
+# flow.from_source(
+#     "https://github.com/jakekaplan/demo-flows.git",
+#     entrypoint="my_flow.py:my_flow"
+# ).deploy(
+#     name="modal-deploy",
+#     work_pool_name="my-modal-pool"
+# )

a3c75550c100ac8a136a914339174b169f3c3b81
jake@prefect.io <jake@prefect.io>
2024-01-16 16:16:04 -0500

    foo

--- my_flow.py
+++ my_flow.py
@@ -1,6 +1,7 @@
 from prefect import flow, task
 import marvin
 import sys
+import os
 
 @task
 def get_version():
@@ -16,3 +17,12 @@ def my_flow():
     v = get_version()
     print_version(v)
     print(marvin.__version__)
+    print(os.environ["FOO"])
+
+# flow.from_source(
+#     source="https://github.com/jakekaplan/demo-flows.git",
+#     entrypoint="my_flow.py:my_flow",
+# ).deploy(
+#     name="my-modal-deploy",
+#     work_pool_name="my-modal-pool",
+# )

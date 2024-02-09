f0fbf4ca03e9d40d5613be85cc32b010a91d556d
jake@prefect.io <jake@prefect.io>
2024-02-09 15:01:21 -0500

    datetime flow

--- my_flow.py
+++ my_flow.py
@@ -1,27 +1,7 @@
-from prefect import flow, task
-import sys
-
-@task
-def get_version():
-    return sys.version
-
-@task
-def print_version(version):
-    print(version)
+from prefect import flow
+from datetime import datetime
 
 
 @flow(log_prints=True)
-def my_flow():
-    print("Hi from a flow!")
-    version = get_version()
-    print_version(version)
-
-
-if __name__ == '__main__':
-    flow.from_source(
-        source="https://github.com/jakekaplan/demo-flows.git",
-        entrypoint="my_flow.py:my_flow",
-    ).deploy(
-        name="my-modal-deploy",
-        work_pool_name="my-modal-pool",
-    )
+def my_flow(datetime: datetime):
+    print("It is currently:", datetime)

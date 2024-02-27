bbd7a68013c3839fd4434d0a8f5d9ca53c813b87
jake@prefect.io <jake@prefect.io>
2024-02-27 12:03:02 -0500

    print hi

--- my_flow.py
+++ my_flow.py
@@ -1,18 +1,6 @@
 from prefect import flow
-from datetime import datetime
 
 
 @flow(log_prints=True)
-def my_flow(datetime: datetime):
-    print("It is currently:", datetime)
-
-
-if __name__ == '__main__':
-    flow.from_source(
-        source="https://github.com/jakekaplan/demo-flows.git",
-        entrypoint="my_flow.py:my_flow",
-    ).deploy(
-        name="my-modal-deploy",
-        work_pool_name="modal-pool-from-cli",
-        enforce_parameter_schema=True
-    )
+def my_flow():
+    print("hi")

bed17cee2a3d19630a9dc0b20bf44052c316ec14
jake@prefect.io <jake@prefect.io>
2024-02-27 13:26:07 -0500

    other stuff

--- my_flow.py
+++ my_flow.py
@@ -1,6 +1,17 @@
 from prefect import flow
+from pydantic import BaseModel
+class SampleSize(BaseModel):
+    total: int | None
+    negative_class: int | None
+    positive_class: int | None
 
 
-@flow(log_prints=True)
-def my_flow():
-    print("hi")
+class AnotherModel(BaseModel):
+    sample_size: SampleSize
+
+@flow
+def sample_size_flow(param: AnotherModel):
+    print(param)
+    print(param.sample_size)
+
+

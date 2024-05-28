69767f3ae480ae9b0966f23618f3442c0df473b1
jake@prefect.io <jake@prefect.io>
2024-05-28 11:06:43 -0400

    fix flow

--- my_flow.py
+++ my_flow.py
@@ -1,11 +1,9 @@
 import time
-import os
 from prefect import flow
 
 
 @flow(log_prints=True)
 def my_flow():
-    print(os.environ)
     print("Starting flow")
     time.sleep(5)
     print("All done")

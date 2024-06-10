66ddd033cdc0f1ef95efac533353d77e19e3ad44
jake@prefect.io <jake@prefect.io>
2024-06-10 14:54:30 -0400

    cuteflow

--- my_flow.py
+++ my_flow.py
@@ -1,15 +1,6 @@
-import time
-import prefect
-import os
 from prefect import flow
 
 
-@flow(log_prints=True)
-def my_flow():
-    print(prefect.__version__)
-    print(os.environ)
-    print("sleeping for 30s")
-    time.sleep(30)
-
-if __name__ == "__main__":
-    my_flow()
+@flow
+def cute_flow(might_delete_later: str = 123):
+    print("I'm a cute flow!")

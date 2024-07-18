020dea2c3199d8546ffaa3d777344026072dd376
jake@prefect.io <jake@prefect.io>
2024-07-18 14:14:23 -0400

    print version

--- my_flow.py
+++ my_flow.py
@@ -1,9 +1,10 @@
-import time
+import prefect
 from prefect import flow
 
 
 @flow(log_prints=True)
 def my_flow():
-    print("Sleeping for 1000s!")
-    time.sleep(10)
-    print("Done sleeping!")
+    print(prefect.__version__)
+
+
+my_flow()

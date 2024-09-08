94d8aa16ae23b387ebdb94e39b3989d7a0f22cbd
jake@prefect.io <jake@prefect.io>
2024-09-08 15:12:14 -0400

    print versions

--- my_flow.py
+++ my_flow.py
@@ -1,8 +1,14 @@
+import prefect
+import fastapi
+import starlette
 from prefect import flow
+
 
 @flow(log_prints=True)
 def my_flow():
-    print("Hello, world!")
+    print(prefect.__version__)
+    print(fastapi.__version__)
+    print(starlette.__version__)
 
 
 if __name__ == '__main__':

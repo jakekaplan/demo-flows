1ab4cad473a28a9f0eb4f2b1796b7e3bb0bf6277
jake@prefect.io <jake@prefect.io>
2024-12-13 12:26:43 -0500

    mason

--- my_flow.py
+++ my_flow.py
@@ -1,11 +1,21 @@
-
-from prefect import flow
-
+from prefect import flow, task, get_run_logger
+from prefect.runner.storage import GitRepository
 
 @flow(log_prints=True)
-def my_flow():
-    print("hello world")
+async def demo_flow(date: str = None):
+    logger = get_run_logger()
+    logger.info(f"Hello World! {date}")
 
 
-if __name__ == '__main__':
-    my_flow()
+
+if __name__ == "__main__":
+    demo_flow.from_source(
+        source=GitRepository(
+            url="https://github.com/masonmenges/mm2-sanbox.git",
+            branch="main"
+            ),
+        entrypoint="flows/demo.py:demo_flow"
+        ).deploy(
+            name="Prefect_managed_deployment_test",
+            work_pool_name="mm2-test"
+        )

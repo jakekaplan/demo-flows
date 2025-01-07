677404c5556e6f7e40a357194151cb578f8255fb
jake@prefect.io <jake@prefect.io>
2025-01-07 18:04:20 -0500

    update

--- my_flow.py
+++ my_flow.py
@@ -1,21 +1,19 @@
-from prefect import flow, get_run_logger
+from prefect import flow
 from prefect.runner.storage import GitRepository
-import time
+
 
 @flow(log_prints=True)
-async def demo_flow(date: str = None):
-    logger = get_run_logger()
-    logger.info(f"Sleeping forever...")
-    time.sleep(10000)
+def demo_flow():
+    print("hi!")
 
 if __name__ == "__main__":
     demo_flow.from_source(
         source=GitRepository(
             url="https://github.com/jakekaplan/demo-flow.git",
             branch="main"
-            ),
+        ),
         entrypoint="my_flow.py"
         ).deploy(
-            name="modal-deployment",
-            work_pool_name="my-modal-pool"
+            name="managed-deploy",
+            work_pool_name="managed-pool"
         )

c95d6c3abf1b2b83a7f5b9a83416f93a0f3050a9
jake@prefect.io <jake@prefect.io>
2025-01-14 13:19:33 -0500

    aksdjfnaskjdf

--- my_flow.py
+++ my_flow.py
@@ -1,4 +1,5 @@
 import prefect
+import os
 from prefect import flow
 from prefect.runner.storage import GitRepository
 
@@ -6,16 +7,17 @@ from prefect.runner.storage import GitRepository
 @flow(log_prints=True)
 def demo_flow():
     print("hi!")
+    print(os.environ)
     print(prefect.__version__)
 
 if __name__ == "__main__":
     demo_flow.from_source(
         source=GitRepository(
-            url="https://github.com/jakekaplan/demo-flow.git",
-            branch="main"
+            url="https://github.com/jakekaplan/demo-flows.git",
+            branch="main",
         ),
-        entrypoint="my_flow.py"
+        entrypoint="my_flow.py:demo_flow"
         ).deploy(
-            name="managed-deploy",
-            work_pool_name="managed-pool"
+            name="coiled-deploy",
+            work_pool_name="coiled-push"
         )

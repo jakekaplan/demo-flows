c948d56a5ddd46173129ae451e79dec461ab9728
jake@prefect.io <jake@prefect.io>
2025-01-17 10:36:39 -0500

    Fix

--- my_flow.py
+++ my_flow.py
@@ -8,29 +8,18 @@ from hashlib import md5
 
 @flow(log_prints=True)
 def demo_flow():
-    print("running prefect: ", prefect.__version__)
-    for i in range(200):
-        now = time.time()
-        while now + 1 > time.time():
-            md5(("a" * 1000000).encode()).hexdigest()
-
-        print(1)
-        emit_event(
-            event="external.resource.pinged",
-            resource={"prefect.resource.id": "my.external.resource"}
-        )
-
+    print("Hi!")
+    print(prefect.__version__)
 
 if __name__ == "__main__":
-    demo_flow()
-    # demo_flow.from_source(
-    #     source=GitRepository(
-    #         url="https://github.com/jakekaplan/demo-flows.git",
-    #         branch="main",
-    #     ),
-    #     entrypoint="my_flow.py:demo_flow"
-    #     ).deploy(
-    #         name="event-test-deploy",
-    #         work_pool_name="managed-pool"
-    #     )
+    demo_flow.from_source(
+        source=GitRepository(
+            url="https://github.com/jakekaplan/demo-flows.git",
+            branch="main",
+        ),
+        entrypoint="my_flow.py:demo_flow"
+        ).deploy(
+            name="event-test-deploy",
+            work_pool_name="managed-pool"
+        )
 

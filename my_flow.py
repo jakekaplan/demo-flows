ba3b351894c8e08ad00f77864d2eff71696961f2
jake@prefect.io <jake@prefect.io>
2025-01-21 15:00:18 -0500

    all

--- my_flow.py
+++ my_flow.py
@@ -1,15 +1,16 @@
-import time
 import prefect
 from prefect.runner.storage import GitRepository
 from prefect import flow
-from prefect.events.utilities import emit_event
-from hashlib import md5
+import os
+
+
 
 
 @flow(log_prints=True)
 def demo_flow():
     print("Hi!")
     print(prefect.__version__)
+    print(os.environ)
 
 if __name__ == "__main__":
     demo_flow.from_source(
@@ -20,6 +21,6 @@ if __name__ == "__main__":
         entrypoint="my_flow.py:demo_flow"
         ).deploy(
             name="event-test-deploy",
-            work_pool_name="managed-pool"
+            work_pool_name="coiled-test"
         )
 

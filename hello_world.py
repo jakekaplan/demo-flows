35d87361d957d83077f9d4c78b1487da25173dc2
jake@prefect.io <jake@prefect.io>
2025-01-28 17:56:41 -0500

    asdfjkasndf

--- hello_world.py
+++ hello_world.py
@@ -2,7 +2,7 @@ from prefect import flow
 import os
 
 @flow
-def hello_world(a: str):
+def hello_world():
     print("hi!")
     for key, value in os.environ.items():
         print(f"{key}: {value}")

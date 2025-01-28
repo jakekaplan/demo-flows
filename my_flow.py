a027f755ab40865c9b498a0742a0cd7ce2d9b041
jake@prefect.io <jake@prefect.io>
2025-01-28 16:20:57 -0500

    demo flows

--- my_flow.py
+++ my_flow.py
@@ -1,31 +1,14 @@
-from prefect import flow, task, get_run_logger
-from prefect.futures import wait
+import prefect
 from prefect.runner.storage import GitRepository
-import time
+from prefect import flow
+import os
 
-@task
-def sleep(interval):
-    time.sleep(interval)
 
-def on_complete(flow, flow_run, state):
-    logger = get_run_logger()
-    logger.info("Done!!!")
-
-def on_failure(flow, flow_run, state):
-    logger = get_run_logger()
-    logger.info("Failed!!!")
-
-def on_crashed(flow, flow_run, state):
-    logger = get_run_logger()
-    logger.info("Crashed!!!")
-
-@flow(on_completion=[on_complete], on_failure=[on_failure], on_crashed=[on_crashed])
-def demo_flow(times, duration):
-    futs = []
-    for i in range(times):
-        futs.append(sleep.submit(duration))
-
-    wait(futs)
+@flow(log_prints=True)
+def demo_flow():
+    print("Hi!")
+    print(prefect.__version__)
+    print(os.environ)
 
 if __name__ == "__main__":
     demo_flow.from_source(
@@ -35,7 +18,6 @@ if __name__ == "__main__":
         ),
         entrypoint="my_flow.py:demo_flow"
         ).deploy(
-            name="sleep-test",
+            name="demo-test",
             work_pool_name="managed-work-pool"
         )
-

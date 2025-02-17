8911c8d59a68445b62f19b2e44ad48a4dfbd0f6c
jake@prefect.io <jake@prefect.io>
2025-02-17 12:53:58 -0500

    push

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,11 @@
+import prefect
+import fastapi
+import starlette
 from prefect import flow
-import os
+
 
 @flow(log_prints=True)
 def hello_world():
-    print("hi!")
-    for key, value in os.environ.items():
-        print(f"{key}: {value}")
+    print("prefect version: ", prefect.__version__)
+    print("fastapi version: ", fastapi.__version__)
+    print("starlette version: ", starlette.__version__)

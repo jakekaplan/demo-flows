1c5bdc6e07fc57b7156883c4692670292da5f31e
jake@prefect.io <jake@prefect.io>
2025-04-03 14:51:04 -0400

    stuff

--- my_flow.py
+++ my_flow.py
@@ -1,6 +1,11 @@
-from time import sleep
+import os
+import prefect
 from prefect import flow
+
 
 @flow(log_prints=True)
 def my_flow():
     print("hello world!")
+    for k, v in os.environ.items():
+        print(f"{k}={v}")
+

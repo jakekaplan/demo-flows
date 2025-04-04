a9dc38faf74b18c9d23fa963b5f13482f78bf350
jake@prefect.io <jake@prefect.io>
2025-04-04 00:10:00 -0400

    run my flow

--- my_flow.py
+++ my_flow.py
@@ -1,11 +1,9 @@
-import os
-import prefect
 from prefect import flow
 
 
 @flow(log_prints=True)
 def my_flow():
     print("hello world!")
-    for k, v in os.environ.items():
-        print(f"{k}={v}")
 
+if __name__ == '__main__':
+    my_flow()

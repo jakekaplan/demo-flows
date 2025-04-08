52911b581026bf5add8eaf8980e47acc322e41a5
jake@prefect.io <jake@prefect.io>
2025-04-08 10:50:32 -0400

    hello world

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,6 @@
-import prefect
-import fastapi
-import starlette
 from prefect import flow
 
 
 @flow(log_prints=True)
 def hello_world():
-    print("prefect version: ", prefect.__version__)
-    print("fastapi version: ", fastapi.__version__)
-    print("starlette version: ", starlette.__version__)
+    print("Hello, world!")

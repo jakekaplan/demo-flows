8fac30d05e80de8747dc6098b0c2a971ccd2f47a
jake@prefect.io <jake@prefect.io>
2025-06-29 16:39:17 -0400

    add a task

--- my_flow.py
+++ my_flow.py
@@ -1,9 +1,17 @@
-import prefect
-from prefect import flow
+
+from prefect import flow, task
+
+@task
+def my_task():
+    import prefect
+    print(prefect.__version__)
 
 @flow(log_prints=True)
 def my_flow():
+    import prefect
     print(prefect.__version__)
+    my_task()
+
 
 
 if __name__ == '__main__':
